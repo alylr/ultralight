471c4756c9356bc5854ccf09cf21105e12df7cf5
alylr <158217830+alylr@users.noreply.github.com>
2024-04-24 17:15:42 -0400

    Add files via upload

---
+++ final_local_website_april24/script.js
@@ -0,0 +1,34 @@
+function updateTimeStatus() {
+  const now = new Date();
+  const hour = now.getHours();
+
+  let status = "";
+
+  if (hour >= 6 && hour < 12) {
+    status = "morning";
+  } else if (hour >= 12 && hour < 17) {
+    status = "afternoon";
+  } else if (hour >= 17 && hour < 21) {
+    status = "evening";
+  } else {
+    status = "night";
+  }
+
+  document.body.setAttribute("data-status", status);
+}
+
+updateTimeStatus(); // Call the function to set initial status
+
+// Update status every second
+setInterval(updateTimeStatus, 1000);
+
+
+
+
+var cards = document.querySelectorAll('.flip-card-inner');
+
+[...cards].forEach((card)=>{
+  card.addEventListener( 'click', function() {
+    card.classList.toggle('is-flipped');
+  });
+});
